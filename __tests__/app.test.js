a941f7e03463f82d39ed95e1e72a433fb27c8fc3
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-02-19 12:28:17 +0000

    Add GET articles by ID endpoint with error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -21,30 +21,63 @@ describe("/api/topics", () => {
       });
   });
 });
-describe('Error handling', () => {
-    test("GET 404: responds with appropriate status and error message for incorrect route", () => {
-        return request(app)
-        .get('/topics')
-        .expect(404)
-        .then(({body: {msg}}) => {
-            expect(msg).toBe('Path not found');
-        })
-    });
+describe("Error handling", () => {
+  test("GET 404: responds with appropriate status and error message for incorrect route", () => {
+    return request(app)
+      .get("/topics")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Path not found");
+      });
+  });
 });
-describe('/api', () => {
-    test('GET 200: responds an object describing all the available endpoints on the API', () => {
-        return request(app)
-        .get('/api')
-        .expect(200)
-        .then(({body: {endpoints}}) => {
-            for (let key in endpoints) {
-                expect(endpoints[key]).toHaveProperty('description');
-                expect(endpoints[key]).toHaveProperty('queries');
-                expect(endpoints[key]).toHaveProperty('exampleResponse');
-                if(['POST', 'PATCH', 'DELETE'].includes(key)) {
-                    expect(endpoints[key]).toHaveProperty('bodyFormat');
-                }
-            }
-        })
-    });
+describe("/api", () => {
+  test("GET 200: responds an object describing all the available endpoints on the API", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body: { endpoints } }) => {
+        for (let key in endpoints) {
+          expect(endpoints[key]).toHaveProperty("description");
+          expect(endpoints[key]).toHaveProperty("queries");
+          expect(endpoints[key]).toHaveProperty("exampleResponse");
+          if (["POST", "PATCH", "DELETE"].includes(key)) {
+            expect(endpoints[key]).toHaveProperty("bodyFormat");
+          }
+        }
+      });
+  });
+});
+describe("/api/articles/:article_id", () => {
+  test("GET 200: responds with an article object for the specified article", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toHaveProperty("author");
+        expect(article).toHaveProperty("title");
+        expect(article).toHaveProperty("article_id");
+        expect(article).toHaveProperty("body");
+        expect(article).toHaveProperty("topic");
+        expect(article).toHaveProperty("created_at");
+        expect(article).toHaveProperty("votes");
+        expect(article).toHaveProperty("article_img_url");
+      });
+  });
+  test("GET 404: responds with correct status and error message when requesting an article that does not exist", () => {
+    return request(app)
+      .get("/api/articles/999999")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not found");
+      });
+  });
+  test("GET 400: responds with correct status and error message when requesting an invalid ID", () => {
+    return request(app)
+    .get('/api/articles/forklift')
+    .expect(400)
+    .then(({body: {msg}}) => {
+        expect(msg).toBe('Bad request');
+    })
+  });
 });
--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -2,7 +2,6 @@ const { readEndpoints } = require("../models/api.models")
 
 exports.getEndpoints = (req, res, next) => {
     readEndpoints().then((endpoints) => {
-        console.log(endpoints);
         res.status(200).send({endpoints});
     })
 }
