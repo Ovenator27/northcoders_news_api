d96ef4105ba1a61fa729b99ccb0b0c55271efab6
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-02-19 13:59:27 +0000

    Add GET articles endpoint to show return all articles

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,11 +1,24 @@
-const db = require('../db/connection')
+const db = require("../db/connection");
 
 exports.selectArticleById = (articleId) => {
-    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
-    .then(({rows}) => {
-        if(rows.length === 0) {
-            return Promise.reject({status: 404, msg: 'Not found'})
-        }
-        return (rows[0])
-    })
-}
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows[0];
+    });
+};
+
+exports.SelectArticles = () => {
+  return db.query(`SELECT 
+  articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count 
+  FROM articles 
+  LEFT JOIN comments On articles.article_id = comments.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC;`)
+  .then(({rows}) => {
+    return rows;
+  });
+};
