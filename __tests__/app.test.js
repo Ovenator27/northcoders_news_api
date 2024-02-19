317f051fbac96f764b1ac3dd00488fc1a30ffe44
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-02-19 11:16:58 +0000

    Add path not found to main

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,8 @@
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const db = require("../db/connection");
-const request = require('supertest');
-const app = require('../app')
+const request = require("supertest");
+const app = require("../app");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -10,14 +10,24 @@ afterAll(() => db.end());
 describe("/api/topics", () => {
   test("GET 200: responds with an array of topic objects", () => {
     return request(app)
-    .get('/api/topics')
-    .expect(200)
-    .then(({body: {topics}}) => {
-        expect(topics.length).toBe(3)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).toBe(3);
         topics.forEach((topic) => {
-            expect(typeof topic.slug).toBe('string');
-            expect(typeof topic.description).toBe('string');
-        })
-    })
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+        });
+      });
   });
+});
+describe('Error handling', () => {
+    test("GET 404: responds with appropriate status and error message for incorrect route", () => {
+        return request(app)
+        .get('/topics')
+        .expect(404)
+        .then(({body: {msg}}) => {
+            expect(msg).toBe('Path not found');
+        })
+    });
 });
