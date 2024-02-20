5218528645a2d2d88c0d2f17e07ef6197ba03a6d
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-02-20 10:01:07 +0000

    Create Delete comments endpoint with associated tests and errors

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,13 +2,14 @@ const { selectArticleById } = require("../models/articles.models");
 const {
   insertComment,
   selectCommentsByArticleId,
+  removeComment,
 } = require("../models/comments.models");
 
 exports.getCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
   return Promise.all([
     selectArticleById(article_id),
-    selectCommentsByArticleId(article_id)
+    selectCommentsByArticleId(article_id),
   ])
     .then((returnedPromises) => {
       res.status(200).send({ comments: returnedPromises[1] });
@@ -19,9 +20,21 @@ exports.getCommentsByArticleId = (req, res, next) => {
 exports.postComment = (req, res, next) => {
   const { article_id } = req.params;
   const { body } = req;
-  return Promise.all([selectArticleById(article_id), insertComment(article_id, body)])
+  return Promise.all([
+    selectArticleById(article_id),
+    insertComment(article_id, body),
+  ])
     .then((returnedPromises) => {
       res.status(201).send({ comment: returnedPromises[1] });
+    })
+    .catch(next);
+};
+
+exports.deleteComment = (req, res, next) => {
+  const { comment_id } = req.params;
+  removeComment(comment_id)
+    .then(() => {
+      res.status(204).send();
     })
     .catch(next);
 };
