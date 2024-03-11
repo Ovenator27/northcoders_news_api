fecaaf5dc5f43f486162b31649103dbacf01332b
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-03-11 10:20:13 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const {
 } = require("./controllers/errors.controllers");
 const apiRouter = require("./routers/api.router");
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api", apiRouter);
 
